0548cc7c05b0b8911e6947175b8c06ba43292aa0
natcha94 <nanthawisit.natacha@gmail.com>
2019-04-30 13:42:29 +0200

    changed back to mysql

--- user-service/src/main/java/no/oslomet/userservice/UserServiceApplication.java
+++ user-service/src/main/java/no/oslomet/userservice/UserServiceApplication.java
@@ -16,8 +16,6 @@ import java.util.List;
 @SpringBootApplication
 public class UserServiceApplication implements CommandLineRunner {
 
-    @Autowired
-    private RoleRepository roleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
@@ -25,7 +23,6 @@ public class UserServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String...args) throws Exception {
-        roleRepository.save(new Role(1, "User"));
-        roleRepository.save(new Role(2, "Admin"));
+
     }
 }
